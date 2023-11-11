5a87d44a43166a166a833c7fa95508c8cc309621
tianxin-personal <96083098+tianxin-personal@users.noreply.github.com>
2023-11-11 16:36:13 -0600

    Add asnyc calls

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,21 +18,21 @@ namespace dotnet_rpg.Controllers
             _characterService = characterService;
         }
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get()
+        public async Task<ActionResult<List<Character>>> Get()
         {
-            return Ok(_characterService.GetAllCharacters());
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id)
+        public async Task<ActionResult<Character>> GetSingle(int id)
         {
-            return Ok(_characterService.GetCharacterById(id));
+            return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newC)
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newC)
         {
-            return Ok(_characterService.AddCharacter(newC));
+            return Ok(await _characterService.AddCharacter(newC));
         }
     }
 }

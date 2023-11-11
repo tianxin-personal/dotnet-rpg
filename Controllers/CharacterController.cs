083c21cfda68ca7565b99a6a4c4a08b214ffda33
tianxin-personal <96083098+tianxin-personal@users.noreply.github.com>
2023-11-11 16:26:02 -0600

    Add Character service

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newC)
+        {
+            return Ok(_characterService.AddCharacter(newC));
+        }
+    }
+}
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static readonly List<Character> characters = new List<Character> {
+            new Character(),
+            new Character{Id = 1, Name = "Sam"}
+        };
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return characters;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if (character is not null)
+            {
+                return character;
+            }
+            throw new Exception("Character not found!");
+        }
+    }
+}
